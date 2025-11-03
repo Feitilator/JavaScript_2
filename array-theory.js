ce8b088016d77dfe9521f62a5dea84f367c9e470
Feitilator <Elsongames63@gmail.com>
2025-11-03 14:42:12 +0500

    Add files via upload

--- array-theory.js
+++ array-theory.js
@@ -17,19 +17,72 @@ const names = ['Владилен', 'Елена','Игорь','Ксения']
 
 // console.log(names)
 
-const greateWoman = 'Елена'
-const index = names.indexOf(greateWoman)
-console.log(index)
-const newNames = names.with(index, 'Елена Великая')
+// const greateWoman = 'Елена'
+// const index = names.indexOf(greateWoman)
+// console.log(index)
+// const newNames = names.with(index, 'Елена Великая')
 // names[index] = 'Елена Великая'
 // console.log(names[index])
 // console.log(newNames)
 // console.log(names)
 
-const capitalNames = names.map(function(name, index){
-  if (index === 1){
-    return 'Eлена Великая'
-  }
-  return name
-})
-console.log(capitalNames)
+// const capitalNames = names.map(function(name, index){
+//   if (index === 1){
+//     return 'Eлена Великая'
+//   }
+//   return name
+// })
+// console.log(capitalNames)
+
+// console.log(names.includes('Игорь'))
+// console.log(names.indexOf('Игорь') !== -1)
+
+const people = [
+  { name : 'Владилен', budget: 4200},
+  { name : 'Елена',    budget: 15100},
+  { name : 'Игорь',    budget: 300},
+  { name : 'Ксения',   budget: 7520},
+]
+
+// console.log(people.indexOf({name: 'Ксения', budget:7520}))
+
+// let findedPerson 
+
+// for (let person of people) {
+//   if(person.budget === 7520){
+//     findedPerson = person
+//   }
+// }
+
+// const findedPerson = people.find(function(person){
+//   return person.budget === 7520
+// })
+
+// const findedPerson = people.findIndex(function(person){
+//   return person.budget === 7520
+// })
+
+// console.log(people[findedPerson])
+
+// const finded = people.find((p) => p.budget === 7520)
+// console.log(finded)
+
+// const filtered = people.filter(function(person) {
+//   return person.budget > 5000
+// })
+
+// let sumBudget = 0
+
+// console.log(filtered)
+// filtered.forEach(function(person){
+//   sumBudget += person.budget 
+// })
+// console.log(sumBudget)
+
+// const sumBudget = people.filter(p => p.budget > 5000).map(p => p.budget).reduce((acc,p) => acc + p,0)
+// console.log(sumBudget)
+
+const string = 'Привет, как дела?'
+const reversed = string.split('').toReversed().join('!').split('').filter(c => c!== '!').join('')
+
+console.log(reversed)
